1b6a230535e97c422f9d94cd6b95878bb870559a
Chen Li <yichen.li0830@gmail.com>
2017-11-14 12:26:29 -0800

    Make categoryList customizable

--- server_singleSentence/docker-version/app/api.py
+++ server_singleSentence/docker-version/app/api.py
@@ -147,18 +147,9 @@ def get_idx_from_text(cl_text, word_idx_map):
     return x
 
 
-def categorization(text):
+def categorization(text, label_list):
     # new data path
     # data_folder = 'processedData'
-    label_list = ['结束询问',
-                  '开头语',
-                  '扣款话术3联系商家',
-                  '收集信息',
-                  '等待致谢',
-                  '扣款话术1亲友儿童',
-                  '问题回答',
-                  '等待提示',
-                  '扣款话术2号码保护']
     # text = '你好，有什么可以帮您唉你好，那个我想问一下咱那个润滑里边那个大众4S店。那个。说不定患者那是要遗弃的还是上海大众，嗯高尔夫是一汽大众，售后是87983734。8798。3734723公星期四签嗯，嗯。'
     # text = '你好，什么可以帮您您好！我想问一下别克凯越换一个前保险杠和一个小小时大约多少钱，那个现在我给您一个4S店售后维修的电话，您去咨询一下4S店这个价格吧，好勒好勒，唉您打一下87527681您可以把我手机上吧这个短信那我给您发一个试一下，吧您稍后看一下手机，看看有没有收到这个短信，好了好了，唉，行行好，那现在再见。嗯。'
     # text = '嗯果然是那个请问咱那个嗯请问压力在吗？刘雅丽，噢他那个测试了不是吧？啊行没事我问一下你我这边咱那个润滑剂克服的，那个咱那个和鞋垫不是撤了吗？然后是不是那个电话都没变，但是迁到中山公园那边那边去了是吧？唉对的，唉他那边有那个就是地址，啊具体地址还有那个电话吗？包括店长咱这边有信息吗，有有是吧？适合和小姐一模一样的吗？嗯那个然后电话对电话是养生对电话一样，谁知张龙，然后你记下地址吧是金三维五稍等稍，等我吧把那个表打开，唉你这有这个表吗？就是最新的。没有没有，是吧，我加一下，吧再加上，把那就是那个店叫什么中山店是吧，就叫中山店。在中山公园店中山公园店啊嗯位置是在金三维五。他就在中山公园吗？啊对，他就在中山花园北门斜对面填写。哈哈镜三为我哪里江山为国中山花园北门，中山花园，北门那边，在对面斜对面，没省医院挺近是吧。唉对，应该是。行，先北门斜对面，然后那个电话我看一下，噢邪见噢噢还是张龙就是876687066288是吧，啊对之前电话没换，行行好勒，谢谢哈好好再见好好再见。'
@@ -204,9 +195,6 @@ def categorization(text):
     loaded_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
     # score = loaded_model.evaluate(x_test, y_test, verbose=0)
     y_test_hat = loaded_model.predict(x_test)
-    # print(y_test[:5])
-    # print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
-    # label_list = build_label_list(data_folder)
 
     response = build_index_label(y_test_hat, label_list)
     print(response)
--- server_singleSentence/docker-version/app/schema.py
+++ server_singleSentence/docker-version/app/schema.py
@@ -1,12 +1,28 @@
 import graphene
 from api import categorization
+import sys
 
 
 class Query(graphene.ObjectType):
-    categorizeSentence = graphene.String(text=graphene.String())
+    categorizeSentence = graphene.String(
+        text=graphene.String(required=True),
+        category_list=graphene.Argument(
+            graphene.List(graphene.String),
+            default_value=['结束询问',
+                           '开头语',
+                           '扣款话术3联系商家',
+                           '收集信息',
+                           '等待致谢',
+                           '扣款话术1亲友儿童',
+                           '问题回答',
+                           '等待提示',
+                           '扣款话术2号码保护']
+        )
+    )
 
-    def resolve_categorizeSentence(self, info, text):
-        return categorization(text)
+    def resolve_categorizeSentence(self, info, text, category_list):
+        print('category_list', category_list, file=sys.stderr)
+        return categorization(text, category_list)
 
 
 schema = graphene.Schema(query=Query)
